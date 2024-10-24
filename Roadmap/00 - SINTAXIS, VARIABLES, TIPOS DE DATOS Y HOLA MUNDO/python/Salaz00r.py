8eefd0a9764c821b01a79054e2f616716b471cd6
Joshua Juarez <75152983+Salaz00r@users.noreply.github.com>
2024-10-23 20:43:12 -0600

    Create Salaz00r.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Salaz00r.py
@@ -0,0 +1,21 @@
+#Comentario 
+"""
+Comentario
+en varias
+Lineas
+"""
+
+name = "Joshua"
+
+name = "Joshua Juarez"
+
+CONSTANT ="MY constant"
+
+int = 1
+bool = True
+bool = False
+cadena = "mi texto"
+float = 1.5
+lenguaje = "python"
+
+print("Mi lenguaje para programar es", lenguaje)
